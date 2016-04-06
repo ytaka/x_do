c8883bc224a5a70c7eb44b60d77613aa3772707c
Alex Boyd <aboyd@instructure.com>
2016-04-06 17:02:43 -0600

    Get rid of struct fields that no longer exist

--- lib/x_do/ffi_lib.rb
+++ lib/x_do/ffi_lib.rb
@@ -12,15 +12,13 @@ module FFILib
   rescue FFI::NotFoundError
     ffi_lib 'libxdo.so'
   end
-  
+
   # :nodoc: lifted from xdo.h
   class XDoContext < FFI::Struct
     layout :xdpy, :pointer,
            :display_name, :string,
            :charcodes, :pointer,
            :charcodes_len, :int,
-           :modmap, :pointer,
-           :keymap, :pointer,
            :keycode_high, :int,
            :keycode_low, :int,
            :keysyms_per_keycode, :int,
@@ -29,7 +27,7 @@ module FFILib
            :debug, :int,
            :features_mask, :int
   end  # class XDo::FFILib::XDoContext
-  
+
   # :nodoc: lifted from xdo.h
   class XDoSearch < FFI::Struct
     layout :title, :pointer,
@@ -45,7 +43,7 @@ module FFILib
            :desktop, :long,
            :limit, :uint
   end  # class XDo::FFILib::XDoSearch
-  
+
   # :nodoc: Window from X11/X.h (Window -> XID -> unsigned long)
   typedef :ulong, :window
   # :nodoc: useconds_t from sys/types.h
